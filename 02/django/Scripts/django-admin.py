d4c56737aed5f20dd9443fc792fbcbfd8b144c73
unknown <robert.masur@live.com>
2020-06-26 22:12:34 +0200

    add django first project

---
+++ 02/django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\robert\einstein\python kurs\web kurs\einstein-web-python\02\django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
